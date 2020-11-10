965aec5ed0b624cc18d5508bbf3d98f604b389c7
pratyu-j <pratyush021j@gmail.com>
2020-11-10 12:20:08 +0530

    2nd commit

--- app/src/main/java/com/mcdoodle/virtualizationprotect/MainActivity.java
+++ app/src/main/java/com/mcdoodle/virtualizationprotect/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mcdoodle.checkvirtualenv.CheckEnv;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String pkg = getPackageName();
+        CheckEnv check = new CheckEnv(pkg, this);
+        check.options();
     }
 }
